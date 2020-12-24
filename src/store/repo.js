306e0d91bbe247d170eb9c8e2b7d4e479ebc5b8e
spd789562 <leo.yicun.lin@gmail.com>
2020-12-24 23:20:23 +0800

    [edit] store query prevent slow network

--- src/store/repo.js
+++ src/store/repo.js
@@ -3,6 +3,7 @@ import { reducerCreator } from './_helper'
 import { assoc, concat } from 'ramda'
 
 export const APPEND_REPO_LIST = 'APPEND_REPO_LIST'
+export const UPDATE_REPO_SEARCH_QUERY = 'UPDATE_REPO_SEARCH_QUERY'
 export const CLEAR_REPO_LIST = 'CLEAR_REPO_LIST'
 
 const initialState = {
@@ -10,21 +11,31 @@ const initialState = {
     total: 0,
     page: 1,
   },
+  query: '',
   data: [],
 }
 
 const reducer = reducerCreator(initialState, {
   [APPEND_REPO_LIST]: (
     { data: originRepo },
-    { total = 0, page = 1, data = [] }
+    { total = 0, page = 1, data = [], query = '' }
   ) => ({
+    query,
     pagination: {
       total,
       page,
     },
     data: concat(originRepo, data),
   }),
-  [CLEAR_REPO_LIST]: () => initialState,
+  [UPDATE_REPO_SEARCH_QUERY]: (state, query) => assoc('query', query, state),
+  [CLEAR_REPO_LIST]: (state) => ({
+    pagination: {
+      total: 0,
+      page: 1,
+    },
+    query: state.query,
+    data: [],
+  }),
 })
 
 export default {
