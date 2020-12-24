8b8a22159bcc5cad2d9aa5a86b218ab9e5b7517d
spd789562 <leo.yicun.lin@gmail.com>
2020-12-24 21:06:26 +0800

    [add] webpack alias

---
+++ config/alias.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+module.exports = {
+  '@api': path.resolve(__dirname, '../src/api'),
+  '@utils': path.resolve(__dirname, '../src/utils'),
+  '@components': path.resolve(__dirname, '../src/components'),
+}
