c80600e3415f9908369f301d0b563db3e4e29745
spd789562 <leo.yicun.lin@gmail.com>
2020-12-24 21:06:55 +0800

    [add] emit funciton

--- src/utils/emit.js
+++ src/utils/emit.js
@@ -1,14 +1,21 @@
+<<<<<<< HEAD
 import { API_EMIT } from '@store/middleware/actions'
 
 export const emit = (type, payload = {}) => ({
+=======
+const emit = (type, payload = {}) => ({
+>>>>>>> 6800d3f... [add] emit funciton
   type,
   payload,
 })
 
+<<<<<<< HEAD
 export const apiEmit = (type, payload = {}) => ({
   type: API_EMIT,
   apiType: type,
   payload,
 })
 
+=======
+>>>>>>> 6800d3f... [add] emit funciton
 export default emit
