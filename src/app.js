9383589c3333dd4327469dd7c5ad430e65057839
spd789562 <leo.yicun.lin@gmail.com>
2020-12-24 21:06:58 +0800

    [add] search component

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 import React from 'react'
+
+/* components */
+import Search from '@components/search'
+
 import './app.css'
 
 function App() {
-  return <div className="app"></div>
+  return (
+    <div className="app">
+      <div className="app-search">
+        <Search />
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -2,8 +2,12 @@ import React, { useCallback } from 'react'
 
 /* store */
 import { useDispatch } from '@store'
+<<<<<<< HEAD
 import { API_GET_REPO } from '@store/middleware/actions'
 import { CLEAR_REPO_LIST } from '@store/repo'
+=======
+import { API_GET_REPO } from '@store/middleware/api'
+>>>>>>> 828530c... [add] search component
 
 /* components */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -11,7 +15,11 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 /* utils */
 import { debounce } from 'throttle-debounce'
+<<<<<<< HEAD
 import { emit, apiEmit } from '@utils/emit'
+=======
+import emit from '@utils/emit'
+>>>>>>> 828530c... [add] search component
 
 import './search.css'
 
@@ -19,8 +27,12 @@ const Search = () => {
   const dispatch = useDispatch()
   const handleSearch = useCallback(
     debounce(300, ({ target: { value } }) => {
+<<<<<<< HEAD
       dispatch(emit(CLEAR_REPO_LIST))
       dispatch(apiEmit(API_GET_REPO, { query: value }))
+=======
+      dispatch(emit(API_GET_REPO, { query: value }))
+>>>>>>> 828530c... [add] search component
     }),
     []
   )
