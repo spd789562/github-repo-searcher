5b188c8f7db202110d0d553289ccbc573d326c2c
spd789562 <leo.yicun.lin@gmail.com>
2020-12-24 22:09:00 +0800

    [edit] fix all wiredness

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
-import './App.css'
+import React from 'react'
+
+/* components */
+import Search from '@components/search'
+import ResultList from '@components/result-list'
+
+import './app.css'
 
 function App() {
-  return <div className="app"></div>
+  return (
+    <div className="app">
+      <div className="app-search">
+        <Search />
+      </div>
+      <ResultList />
+    </div>
+  )
 }
 
 export default App
