f1230cabfca5131b700254bb61d77e7495c9ebed
spd789562 <leo.yicun.lin@gmail.com>
2020-12-24 21:06:38 +0800

    [add] store structure

--- config/alias.js
+++ config/alias.js
@@ -4,4 +4,5 @@ module.exports = {
   '@api': path.resolve(__dirname, '../src/api'),
   '@utils': path.resolve(__dirname, '../src/utils'),
   '@components': path.resolve(__dirname, '../src/components'),
+  '@store': path.resolve(__dirname, '../src/store'),
 }
--- src/app.js
+++ src/app.js
@@ -1,12 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import './App.css'
-import { APIGetRepository, APIGetRateLimit } from '@api'
 
 function App() {
-  useEffect(() => {
-    APIGetRepository().then(console.log)
-    APIGetRateLimit().then(console.log)
-  })
   return <div className="App"></div>
 }
 
