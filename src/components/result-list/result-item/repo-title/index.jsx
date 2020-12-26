0c8d6134f98d8a354a81ed5ec942117fd4d3ed80
spd789562 <leo.yicun.lin@gmail.com>
2020-12-26 16:40:21 +0800

    [edit] result item style

--- src/components/result-list/result-item/repo-title/index.jsx
+++ src/components/result-list/result-item/repo-title/index.jsx
@@ -5,7 +5,7 @@ import './repo-title.css'
 const RepoTitle = ({ children = '', link, placeholder }) => (
   <h4 className={`repo-title ${placeholder ? 'repo-title__placeholder' : ''}`}>
     {children && (
-      <a href={link} title={children}>
+      <a href={link} title={children} target="_blank">
         {children}
       </a>
     )}
